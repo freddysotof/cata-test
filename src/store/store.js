ab56c82f334e2be6686d5a53ea0aafd5805ccd51
Freddy Soto <fsoto@elcatador.com>
2023-01-27 14:24:10 -0400

    fix store reference

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {  authSlice, authApi,   uiSlice } from "./";
+import {  authSlice,    uiSlice } from "./";
 
 
 export const store = configureStore({
     reducer:{
         auth:authSlice.reducer,
-        [authApi.reducerPath]:authApi.reducer,
+        // [authApi.reducerPath]:authApi.reducer,
         ui:uiSlice.reducer
     },
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
         serializableCheck:false
     })
-    .concat(authApi.middleware)
+    .concat()
 })
